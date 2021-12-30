501874a2c64de610a6db47ef16be670cd0ac48b5
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-12-30 08:39:18 -0500

    updates rum init

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,17 @@ import App from './App';
 import { datadogRum } from '@datadog/browser-rum';
 
 datadogRum.init({
-  applicationId: `${import.meta.env.REACT_APP_DD_APPLICATION_ID}`,
-  clientToken: `${import.meta.env.REACT_APP_DD_CLIENT_TOKEN}`,
-  site: 'datadoghq.com',
-  service: 'storedog-microsite',
-  version: '1.1',
-  env: `${import.meta.env.REACT_APP_DD_ENV}`,
+  applicationId: `${
+    import.meta.env.REACT_APP_DD_APPLICATION_ID ||
+    'DD_APPLICATION_ID_PLACEHOLDER'
+  }`,
+  clientToken: `${
+    import.meta.env.REACT_APP_DD_CLIENT_TOKEN || 'DD_CLIENT_TOKEN_PLACEHOLDER'
+  }`,
+  site: `${import.meta.env.REACT_APP_DD_SITE || 'datadoghq.com'}`,
+  service: `${import.meta.env.REACT_APP_DD_SERVICE || 'discounts-frontend'}`,
+  version: `${import.meta.env.REACT_APP_DD_VERSION || '1.0.0'}`,
+  env: `${import.meta.env.REACT_APP_DD_ENV || 'production'}`,
   sampleRate: 100,
   trackInteractions: true,
   defaultPrivacyLevel: 'mask-user-input',
